4d5dc1982fb378640ee58a24a7a2551bae077a87
miramiramirami <xeless@mail.ru>
2026-02-22 22:34:15 +0500

    first router + schemas

---
+++ backend/routers/story.py
@@ -0,0 +1,50 @@
+import uuid
+from typing import Optional
+from datetime import datetime
+from fastapi import APIRouter, Depends, HTTPException, Cookie, Response, BackgroundTasks
+from sqlalchemy.orm import Session
+
+from db.database import get_db, SessionLocal
+from models.story import Story, StoryNode
+from models.job import StoryJob
+from schemas.story import (
+    CompleteStoryResponse,
+    CompleteStoryNodeResponse,
+    CreateStoryRequest
+)
+from schemas.job import StoryJobResponse
+
+
+router = APIRouter(prefix='/stories', tags=["stories"])
+
+def get_session_id(session_id: Optional[str] = Cookie(None)):
+    if not session_id:
+        session_id = str(uuid.uuid4())
+    return session_id
+
+
+@router.post('/create', response_model=StoryJobResponse)
+def create_story(
+        request: CreateStoryRequest,
+        background_tasks: BackgroundTasks,
+        response: Response,
+        session_id: str = Depends(get_session_id),
+        db: Session = Depends(get_db)
+):
+    response.set_cookie(key="session_id", value=session_id, httponly=True)
+
+    job_id = str(uuid.uuid4())
+
+    job = StoryJob(
+        job_id=job_id,
+        session_id=session_id,
+        theme=request.theme,
+        status='pending'
+    )
+    db.add(job)
+    db.commit()
+
+    #TODO: add background tasks
+
+    return job
+
--- backend/schemas/story.py
+++ backend/schemas/story.py
@@ -10,12 +10,34 @@ class StoryOptionsSchema(BaseModel):
 class StoryNodeBase(BaseModel):
     content: str
     is_ending: bool = False
-    is_winnig_ending: bool = False
+    is_winning_ending: bool = False
 
 
 class CompleteStoryNodeResponse(StoryNodeBase):
     id: int
     options: List[StoryOptionsSchema] = []
+
+    class Config:
+        from_attributes = True
+
+
+class StoryBase(BaseModel):
+    title: str
+    session_id: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+
+class CreateStoryRequest(BaseModel):
+    theme: str
+
+
+class CompleteStoryResponse(StoryBase):
+    id: int
+    created_at: datetime
+    root_node: CompleteStoryNodeResponse
+    all_notes: Dict[int, CompleteStoryNodeResponse]
 
     class Config:
         from_attributes = True
